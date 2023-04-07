eb5984e3b0d3d98cdddc8cab90cec59dc2a58f6e
KhushiKasaudhan <khushikasaudhan8041@gmail.com>
2023-04-07 11:43:09 +0530

    Commit Files

---
+++ bubble_tea_app/lib/models/drink.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Drink{
+  final String name;
+  final String price;
+  final String imagePath;
+
+  Drink({
+    required this.name,
+    required this.price,
+    required this.imagePath
+     });
+}
